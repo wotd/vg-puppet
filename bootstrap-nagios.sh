f484d2d549b7f16079cf5ddb1496b44e084ae78b
wotd <ngebicki@gmail.com>
2015-10-19 07:28:41 +0200

    fixed locale errors

--- bootstrap-nagios.sh
+++ bootstrap-nagios.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 dpkg -i puppetlabs-release-trusty.deb
+locale-gen pl_PL pl_PL.UTF-8
+dpkg-reconfigure locales
 apt-get update
 apt-get -y install puppet
 echo "192.168.50.2 puppet" >> /etc/hosts
@@ -8,4 +10,3 @@ echo "192.168.50.40 lamp" >> /etc/hosts
 echo "192.168.50.4 nagios" >> /etc/hosts
 sed -i".bak" '/^templatedir=.*/d' /etc/puppet/puppet.conf
 puppet agent --enable
-
